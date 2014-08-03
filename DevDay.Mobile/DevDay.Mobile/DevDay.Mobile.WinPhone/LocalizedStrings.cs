9412ae809891f65a0cad706deaac5b94a1e8f1c7
= <=>
2014-08-04 01:06:24 +0200

    project initialization

---
+++ DevDay.Mobile/DevDay.Mobile/DevDay.Mobile.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using DevDay.Mobile.WinPhone.Resources;
+
+namespace DevDay.Mobile.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
